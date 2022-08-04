3f8cf91d55ded370c44aa3b37466e1e14225a3ff
jsxu <45815139+hzjsxu@users.noreply.github.com>
2022-08-04 13:50:50 +0800

    Update chromunity.R

--- R/chromunity.R
+++ R/chromunity.R
@@ -815,8 +815,8 @@ chromunity = function(concatemers, resolution = 5e4, region = si2gr(concatemers)
             suppressWarnings({
                 these.bins = binmap[.(win), ]
                 cc = tryCatch({concatemer_communities(concatemers %Q% (binid %in% these.bins$binid), 
-                                         k.knn = 25, max.size = 2^31-1, k.min = 5, 
-                                         seed = 42, verbose = TRUE>1)},
+                                         k.knn = k.knn, max.size = max.size, k.min = k.min, 
+                                         seed = seed, verbose = verbose>1)},
                       warning=function(x){2;print('Warning...')})  
               ## cc = concatemer_communities(concatemers %Q% (binid %in% these.bins$binid), k.knn = k.knn, max.size = max.size, k.min = k.min, seed = seed, verbose = verbose>1)
                 if (length(cc))
